12990aa3671cbfaaa6f7b99e1173e40a0fe4875e
Ney Congjuico <ncongjuico@yahoo.com>
2017-02-13 22:48:34 -0800

    02/13/2017 10:48PM
    + Added string returns for food and snake positions,
    * Modified string return for Board so it follows protocol
    * Snake direction need to be sent to client

--- server/SERVERVS/SERVERVS/snake.cpp
+++ server/SERVERVS/SERVERVS/snake.cpp
@@ -1,29 +1,39 @@
 #include <vector>
+#include <string>
 #include <utility>
 #include "stdlib.h"
 
 class Snake {
 private:
-	enum directions {
-		UP, DOWN, LEFT, RIGHT
-	};
-	directions currDir = UP;
+	int currDir = 0; //0 - UP, 1 - DOWN, 2 = LEFT, 3 - RIGHT
 	std::vector<std::pair<int, int>> snakeArray;
-	std::pair<int, int> last;
-
-	Snake() {};
+	std::pair<int, int> head;
+	std::pair<int, int> tail;
 
 public:
-	void changeDirection(int dir) {
-		
+
+	void setHead(int x, int y) {
+		head = std::make_pair(x, y);
 	}
 
-	void insert() {
+	void setTail(int x, int y) {
+		tail = std::make_pair(x, y);
 
 	}
 
-	void remove() {
+	//RETURNS SNAKE HEAD AND TAIL AS "x,y-x,y" (First pair is head, second pair is tail)
+	std::string getPos() {
+		std::string h = std::to_string(head.first) + "," + std::to_string(head.second);
+		std::string t = std::to_string(tail.first) + "," + std::to_string(tail.second);
+		return (h + "-" + t);
+	}
 
+	void setDirection(int dir) {
+		currDir = dir;
+	}
+
+	std::string getDirection() {
+		return std::to_string(currDir);
 	}
 	
 };
@@ -37,20 +47,20 @@ public:
 class Board {
 private:
 	int board[20][20];
+	std::string boardLayout;
 
 	//constructor
 	Board() {
 		int row = 20;
 		int col = 20;
-		std::string boardLayout;
 
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < col; j++) {
-				if (i == 0 || i == row-1) {
+				if (i == 0 || i == row - 1) {
 					board[i][j] = 1;
 				}
 				else if (j == 0 || j == col - 1) {
-					board[i][j] = 1; 
+					board[i][j] = 1;
 				}
 				else {
 					board[i][j] = 0;
@@ -61,7 +71,10 @@ private:
 		// CREATE THE STRING TO BE SENT OUT TO THE CLIENTS FOR BOARD DRAWING
 		for (int i = 0; i < row; ++i) {
 			for (int j = 0; j < col; ++j) {
-				boardLayout += board[i][j];
+				boardLayout += std::to_string(board[i][j]);
+			}
+			if (i != row - 1) { //doesn't put an "n" at the end of the string
+				boardLayout += "n"; //adds an "n" at the end of every row
 			}
 		}
 	}
@@ -72,20 +85,30 @@ public:
 	void setValue(int x, int y, int value) {
 		board[x][y] = value;
 	}
+
+
+	std::string getBoard() {
+		return boardLayout;
+	}
 };
 
 class GameState {
 private:
 	Board board;
-	Snake snake1;
-	Snake snake2;
+	Snake snake1; //p1 snake instance
+	Snake snake2; //p2 snake instance
+	std::pair<int, int> foodXY; //stores current food location
 
 public:
 	void setFood() {
-		int randx = rand() % 20;
-		int randy = rand() % 20;
+		foodXY = std::make_pair(rand() % 20, rand() % 20);
+		
+		board.setValue(foodXY.first, foodXY.second, 2); //2 for food
+	}
 
-		board.setValue(randx, randy, 2); //2 for food
+	//RETURNS CURRENT FOOD LOCATION AS "x,y"
+	std::string getFood() {
+		return (std::to_string(foodXY.first) + "," + std::to_string(foodXY.second));
 	}
 
 	bool checkWall(int x, int y) {
